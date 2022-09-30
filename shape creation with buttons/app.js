c43075e784925f0517bf354bb45e913397ced70a
iqra yousuf <iqrayousufkhanani@gmail.com>
2022-10-01 02:16:36 +0500

    first commit

---
+++ shape creation with buttons/app.js
@@ -0,0 +1,13 @@
+function square(){
+   box = document.getElementById("squares");
+   box.className= "circles"
+}
+function rectangle(){
+    boxes = document.getElementById("box");
+    boxes.className = "squares"
+ }
+ function circle(){
+     box = document.getElementById("rectangle");
+    box.className = "rectangles"
+ }
+ 
